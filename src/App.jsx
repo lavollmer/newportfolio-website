8fee447db16dd4a5ac991d2b6ff139ded2828d61
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-21 15:53:34 -0600

    debugging chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,17 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react";
-
-import {
-  ChakraBaseProvider,
-  extendBaseTheme,
-  theme as chakraTheme,
-} from "@chakra-ui/react";
-
-const { Button } = chakraTheme.components;
-
-const theme = extendBaseTheme({
-  components: {
-    Button,
-  },
-});
-
 function App() {
   return (
     <>
-      <ChakraProvider theme={theme}>
-        <div>
-          <p>Hello</p>
-          <Header />
-          <Showcase />
-          <Skills />
-          <Contact />
-          <Footer />
-        </div>
-      </ChakraProvider>
+      <div>
+        <p>Hello</p>
+        <Header />
+        <Showcase />
+        <Skills />
+        <Contact />
+        <Footer />
+      </div>
     </>
   );
 }
