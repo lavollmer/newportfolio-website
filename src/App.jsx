0cb7e776fce1a19ce3242e0ad23039ec5d9bf93c
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-21 17:46:13 -0600

    adding old code for a NavBar with react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
-import Showcase from "./components/Showcase";
+import NavBar from "./components/NavBar";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Project from "./components/Project";
 import { Box } from "@chakra-ui/react";
-import { Divider } from "@chakra-ui/react";
+import { useState } from "react";
 
 //mobile responsive design
 const breakpoints = {
@@ -23,6 +23,7 @@ function App() {
     <>
       <div>
         <Box w={[300, 400, 500]}>
+          <NavBar />
           <Header />
           <Project />
           <Skills />
