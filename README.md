2f13d75cda28faa3e3dc01e6651a083abddc6f8e
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-21 18:30:04 -0600

    adding a SideNav bar

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Credits
 
 Background Image: https://unsplash.com/photos/silhouette-photography-of-person-oMpAz-DN-9I
+
+Sidenav Bar Inspiration: https://www.youtube.com/watch?v=U121KdsZ8QE
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Divider } from "@chakra-ui/react";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <div>
+      <Flex
+        pos="sticky"
+        left="5px"
+        h="95vh"
+        marginTop="2.5vh"
+        boxShadow="0 4px 12px 0 rgba(0,0,0,0.5)"
+        w="200px"
+        justify-content="space-between"
+      >
+        <Flex></Flex>
+        <Flex p="5%" flexDir="column" w="100%" alignItems="flex-start" mb={4}>
+          <Divider />
+        </Flex>
+      </Flex>
+    </div>
+  );
 };
 
 export default Sidenav;
