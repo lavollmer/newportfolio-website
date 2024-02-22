f9b19e6fd19a1dbeb728ce0b79579027d206b6c2
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-21 18:39:32 -0600

    adding Sidenav bar

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Divider } from "@chakra-ui/react";
+import { Divider, Text, Flex, Heading, IconButton } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+import { useState } from "react";
 
 const Sidenav = () => {
+  const [navSize, changeNavSize] = useState("large")
   return (
     <div>
       <Flex
@@ -13,9 +17,26 @@ const Sidenav = () => {
         w="200px"
         justify-content="space-between"
       >
-        <Flex></Flex>
+        <Flex p="5%" flexDir="column" alignItems="flexStart" as="nav">
+          <IconButton
+            background="none"
+            mt={5}
+            _hover={{ background: "none" }}
+            icon={<FiMenu />}
+            onClick={{} =>{}}
+          />
+        </Flex>
         <Flex p="5%" flexDir="column" w="100%" alignItems="flex-start" mb={4}>
           <Divider />
+          <Flex mt={4} align="center">
+            <FaStar color="blue" />
+            <Flex flexDir="column" ml={4}>
+              <Heading as="h3" size="sm" color="gray">
+                Laura Vollmer
+              </Heading>
+              <Text>Admin</Text>
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </div>
