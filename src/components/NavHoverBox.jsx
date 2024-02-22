0671b9d5e78c9b20b52181cd124b5c754af45837
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-21 19:38:35 -0600

    updated sidenav bar

--- src/components/NavHoverBox.jsx
+++ src/components/NavHoverBox.jsx
@@ -4,7 +4,17 @@ import { Icon, Flex, Heading, Text } from "@chakra-ui/react";
 const NavHoverBox = ({ icon, title, description }) => {
   return (
     <>
-      <Flex />
+      <Flex
+        pos="absolute"
+        //dynamically calculate the margin top
+        mt="calc(100px - 7.5px)"
+        ml="-10px"
+        width={0}
+        height={0}
+        borderTop="10px solid transparent"
+        borderBottom="10px solid transparent"
+        borderRight="10px solid green"
+      />
       <Flex
         h={200}
         w={200}
