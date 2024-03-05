301c04f9c59e0f93352b1124f5aab51826e5fd81
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-05 15:37:46 -0600

    added project information

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,20 +7,32 @@ import {
   Text,
   CardBody,
   Button,
+  Box,
 } from "@chakra-ui/react";
+
+import { Image } from "@chakra-ui/react";
 
 const Project = () => {
   return (
     <>
       <Card>
         <CardHeader>
-          <Heading size="sm">Portfolio Website</Heading>
+          <Heading size="sm">Giftify Website</Heading>
         </CardHeader>
         <CardBody>
-          <Text></Text>
+          <Box boxSize="sm">
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+          </Box>
+          <Text> This project is based on the Giftify website.</Text>
         </CardBody>
         <CardFooter>
-          <Button></Button>
+          <Button>Github Code</Button>
+          <Button>Website</Button>
         </CardFooter>
       </Card>
     </>
