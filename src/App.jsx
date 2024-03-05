a9c371069064ae381d75e1ad4f89b041d3f6fbda
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-05 14:51:30 -0600

    cleaning code up and trying to get sidenav bar to overlay main page

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,12 @@ import Skill from "./components/Skill";
 import React from "react";
 import { useEffect } from "react";
 
-//mobile responsive design
-// const breakpoints = {
-//   base: "0em", // 0px
-//   sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
-//   md: "48em", // ~768px
-//   lg: "62em", // ~992px
-//   xl: "80em", // ~1280px
-//   "2xl": "96em", // ~1536px
-// };
-
 function App() {
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const sidenav = document.querySelector(".sidenav");
-  //     if (window.pageYOffset > 0) {
-  //       sidenav.classList.add("fixed");
-  //     } else {
-  //       sidenav.classList.remove("fixed");
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
   return (
     <>
       <div>
         <Main />
-        <SideNav />
+        <SideNav className="sidenav" />
         <Project />
         <About />
         <Skill />
